5c25cb5d1a7124794ac7aceed9ff7e27df801582
t2hit <t.matsukusa@gmail.com>
2023-09-01 18:18:34 +0900

    計算アプリ課題完了

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,5 +8,27 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    public function result($num1, $calc, $num2)
+    {
+        switch ($calc) {
+            case 'addition':
+                $result = $num1 + $num2;
+                break;
+            case 'subtraction':
+                $result = $num1 - $num2;
+                break;
+            case 'multiplication':
+                $result = $num1 * $num2;
+                break;
+            case 'division':
+                $result = $num1 / $num2;
+                break;
+            default:
+                $result = '計算出来ません';
+                break;
+        }
+        return view('calcs.result', ['result' => $result]);
+    }
+
     use AuthorizesRequests, ValidatesRequests;
 }
